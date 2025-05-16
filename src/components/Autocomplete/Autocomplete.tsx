2af189c7d4c406703a9eb15fce1da9fc43c8dd0a
William Grant <williamg3575467@gmail.com>
2025-05-16 16:32:17 +0100

    Improve autocomplete to prevent submission before selection

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,20 +1,20 @@
 interface AutocompleteProps {
-  autocomplete: string[],
-  setAutocomplete: React.Dispatch<React.SetStateAction<string[]>>,
+  autocomplete: { selected: boolean, values: string[] },
+  setAutocomplete: React.Dispatch<React.SetStateAction<{ selected: boolean, values: string[] }>>,
   setGuess: React.Dispatch<React.SetStateAction<string>>
 }
 
 const Autocomplete = ({ autocomplete, setAutocomplete, setGuess }: AutocompleteProps) => {
   const clickHandler = (name: string) => {
     setGuess(name);
-    setAutocomplete([]);
+    setAutocomplete({ selected: true, values: [] });
   }
 
   return (
     <div>
       {
-        autocomplete &&
-        autocomplete.map((cardName) => {
+        autocomplete.values &&
+        autocomplete.values.map((cardName) => {
           return (
             <div key={cardName} onClick={() => clickHandler(cardName)}>{cardName}</div>
           )
--- src/components/GuessForm/GuessForm.tsx
+++ src/components/GuessForm/GuessForm.tsx
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { scryfall } from "../../utils/scryfall";
 import Autocomplete from "../Autocomplete/Autocomplete";
 
 const GuessForm = () => {
   const [guess, setGuess] = useState<string>("");
-  const [autocomplete, setAutocomplete] = useState<string[]>([]);
+  const [autocomplete, setAutocomplete] = useState<{ selected: boolean, values: string[] }>({ selected: false, values: [] });
 
   const changeHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
     setGuess(e.target.value);
 
     if (e.target.value.length >= 3) {
-      setAutocomplete(await scryfall.autocomplete(e.target.value))
+      const values = await scryfall.autocomplete(e.target.value);
+      // If user changes input before submitting, selected should be false
+      setAutocomplete(({ selected: false, values }));
     }
   }
 
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setGuess("");
+    setAutocomplete(prev => ({ ...prev, selected: false }));
     // Submit logic here
   }
+
+  // This should be a ternary on the className of the button
+  // because this would need a ref
+  useEffect(() => {
+    if (autocomplete.selected) {
+      // Enable the submit button
+    } else {
+      // Disable the submit button
+    }
+  }, [guess])
 
   return (
     <form onSubmit={submitHandler}>
