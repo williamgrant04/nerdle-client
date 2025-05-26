8ef6fdcf62c3ca22364a9efd81e828447543f52e
William Grant <williamg3575467@gmail.com>
2025-05-26 13:02:50 +0100

    Context to store guess and comparison data

---
+++ src/context/GuessContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+interface Guess {
+  guess: Card,
+  comparison: Comparison
+}
+
+// Guesses state will contain an object with the guess and the comparison data
+const guessContext = createContext({
+  guesses: [] as Guess[],
+  setGuesses: (() => {}) as React.Dispatch<React.SetStateAction<Guess[]>>,
+})
+
+export const GuessContextProvider = ({ children }: { children: React.JSX.Element }) => {
+  const [guesses, setGuesses] = useState<Guess[]>([])
+
+  return (
+    <guessContext.Provider value={{
+      guesses,
+      setGuesses
+    }}>
+      { children }
+    </guessContext.Provider>
+  )
+}
+
+export default guessContext
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { GuessContextProvider } from './context/GuessContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GuessContextProvider>
+      <App />
+    </GuessContextProvider>
   </StrictMode>,
 )
