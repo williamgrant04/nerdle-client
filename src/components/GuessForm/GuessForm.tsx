82a8dd7f3b74f89b52dab16a037842a62220e8ed
William Grant <williamg3575467@gmail.com>
2025-05-26 13:20:37 +0100

    Access context and store guess & comparison data

--- src/components/GuessForm/GuessForm.tsx
+++ src/components/GuessForm/GuessForm.tsx
@@ -1,26 +1,39 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { scryfall } from "../../utils/scryfall";
 import Autocomplete from "../Autocomplete/Autocomplete";
+import { comparison } from "../../utils/comparison";
+import guessContext from "../../context/GuessContext";
 
 const GuessForm = () => {
   const [guess, setGuess] = useState<string>("");
   const [autocomplete, setAutocomplete] = useState<{ selected: boolean, values: string[] }>({ selected: false, values: [] });
+  const { setGuesses } = useContext(guessContext);
 
   const changeHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
     setGuess(e.target.value);
 
     if (e.target.value.length >= 3) {
+      // ? Add a debounce
       const values = await scryfall.autocomplete(e.target.value);
       // If user changes input before submitting, selected should be false
       setAutocomplete(({ selected: false, values }));
     }
   }
 
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setGuess("");
     setAutocomplete(prev => ({ ...prev, selected: false }));
-    // Submit logic here
+    try {
+      // Need some form of validation or actual error handling
+      const card = await scryfall.getCardByName(guess);
+      const compared = await comparison.compare(card);
+      console.log(compared);
+      
+      if (card) setGuesses(prev => [...prev, { guess: card, comparison: compared }]);
+    } catch (error) {
+
+    }
   }
 
   // This should be a ternary on the className of the button
