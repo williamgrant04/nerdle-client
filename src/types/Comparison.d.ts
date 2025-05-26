2b4a16d248e4e824265c5b36ef18bef45c110611
William Grant <williamg3575467@gmail.com>
2025-05-26 13:04:24 +0100

    Types for Card from scryfall API and comparison

---
+++ src/types/Comparison.d.ts
@@ -0,0 +1,10 @@
+interface Comparison {
+  name: boolean,
+  mana: "higher" | "lower" | "same",
+  rarity: "higher" | "lower" | "same",
+  type: boolean,
+  subtype: boolean,
+  set: boolean,
+  released_at: "higher" | "lower" | "same",
+  colors: string[] // This is an array of the colors that match
+}
