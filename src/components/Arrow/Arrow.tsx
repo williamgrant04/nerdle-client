f10ed61677d534990c65187891a14dd425f31ee1
William Grant <williamg3575467@gmail.com>
2025-05-29 11:23:53 +0100

    Add arrow component that changes direction from props

---
+++ src/components/Arrow/Arrow.tsx
@@ -0,0 +1,20 @@
+import classes from "./Arrow.module.css"
+
+interface ArrowProps {
+  direction: "higher" | "lower" | "same";
+}
+
+const Arrow = ({ direction }: ArrowProps) => {
+  return (
+    <>
+      {
+        direction !== "same" &&
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className={ direction === "lower" ? classes.lower : "" }>
+          <path d="m12 6.586-8.707 8.707 1.414 1.414L12 9.414l7.293 7.293 1.414-1.414L12 6.586z"/>
+        </svg>
+      }
+    </>
+  )
+}
+
+export default Arrow;
