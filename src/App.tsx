86d6a1f3f697296d29a0782579c12147f6de4b73
William Grant <williamg3575467@gmail.com>
2025-05-29 19:23:04 +0100

    Finish guesses component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <GuessForm />
-      <Guesses />
+      <Guesses colorblind={false}/>
     </>
   )
 }
--- src/components/Guesses/Guesses.tsx
+++ src/components/Guesses/Guesses.tsx
@@ -2,12 +2,33 @@ import { useContext } from "react";
 import guessContext from "../../context/GuessContext";
 import Arrow from "../Arrow/Arrow";
 import classes from "./Guesses.module.css";
+import colors from "./Colors.module.css"
 
-const Guesses = () => {
+/*
+  This component is disgusting, I need to refactor it (later)
+*/
+const Guesses = ({ colorblind }: { colorblind: boolean }) => {
   const { guesses } = useContext(guessContext);
 
+  const mapColor = (color: string) => {
+    switch (color) {
+      case "W":
+        return colors.white;
+      case "U":
+        return colors.blue;
+      case "B":
+        return colors.black;
+      case "R":
+        return colors.red;
+      case "G":
+        return colors.green;
+      default:
+        return  colors.white;
+    }
+  }
+
   return (
-    <div className="center-children">
+    <div className="center-children" style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
       {
         guesses.map(({ guess, comparison }) => (
           <div key={guess.id} className={ classes.guessWrapper }>
@@ -31,20 +52,6 @@ const Guesses = () => {
                 </div>
               </div>
 
-              <div className={ classes.type }>
-                <p>Type</p>
-                <div>
-                  <p>{guess.type_line.split(" ")[0]}</p>
-                </div>
-              </div>
-
-              <div className={ classes.subtype }>
-                <p>Subtype</p>
-                <div>
-                  <p>{guess.type_line.split(" ")[2]}</p>
-                </div>
-              </div>
-
               <div className={ classes.set }>
                 <p>Set</p>
                 <div>
@@ -54,11 +61,33 @@ const Guesses = () => {
                 </div>
               </div>
 
-              <div className={ classes.colors }>
+              <div className={ classes.colorsWrapper }>
                 <p>Colors</p>
-                <p>{guess.colors.join(", ")}</p>
-                <p>{`${comparison.colors.remaining}`}</p>
-                <p>comp: {comparison.colors.matching.join(", ")}</p>
+                <div className={ `${classes.colors} ${colorblind && colors.colorblind}` }>
+                  {
+                    guess.colors.map((color, index) => (
+                      <span key={index} className={ `${mapColor(color)} ${colors.color}` }>
+                        {color}
+                      </span>
+                    ))
+                  }
+                </div>
+                {/* <p>{`${comparison.colors.remaining}`}</p>
+                <p>comp: {comparison.colors.matching.join(", ")}</p> */}
+              </div>
+
+              <div className={ classes.type }>
+                <p>Type</p>
+                <div>
+                  <p>{guess.type_line.split("—")[0]?.trim() ?? "—"}</p>
+                </div>
+              </div>
+
+              <div className={ classes.subtype }>
+                <p>Subtype</p>
+                <div>
+                  <p>{guess.type_line.split("—")[1]?.trim() ?? "—"}</p>
+                </div>
               </div>
             </div>
           </div>
