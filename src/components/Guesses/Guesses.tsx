55ee1a55b01f0863e711e3ccf3dfbcad9a0168c4
William Grant <williamg3575467@gmail.com>
2025-05-29 11:24:45 +0100

    Update Guesses structure for styles

--- src/components/Guesses/Guesses.tsx
+++ src/components/Guesses/Guesses.tsx
@@ -1,19 +1,63 @@
 import { useContext } from "react";
 import guessContext from "../../context/GuessContext";
+import Arrow from "../Arrow/Arrow";
+import classes from "./Guesses.module.css";
 
 const Guesses = () => {
   const { guesses } = useContext(guessContext);
+
   return (
-    <>
+    <div className="center-children">
       {
-        guesses.map((guess) => (
-          <div key={guess.guess.id}>
-            <h2>{guess.guess.name}</h2>
-            <p>Comparison: {JSON.stringify(guess.comparison)}</p>
+        guesses.map(({ guess, comparison }) => (
+          <div key={guess.id} className={ classes.guessWrapper }>
+            <h2>{guess.name}</h2>
+            <div className={ classes.guess }>
+              <img src={guess.image_uris?.png} height="200" alt={guess.name} className={ classes.guessImage }/>
+
+              <div className={ classes.mana }>
+                <p>Mana</p>
+                <div>
+                  <p>{guess.cmc}</p>
+                  <Arrow direction={comparison.mana}/>
+                </div>
+              </div>
+
+              <div className={ classes.rarity }>
+                <p>Rarity</p>
+                <div>
+                  <p>{guess.rarity}</p>
+                  <Arrow direction={comparison.rarity} />
+                </div>
+              </div>
+
+              <div className={ classes.type }>
+                <p>Type</p>
+                <p>{guess.type_line.split(" ")[0]}</p>
+              </div>
+
+              <div className={ classes.subtype }>
+                <p>Subtype</p>
+                <p>{guess.type_line.split(" ")[2]}</p>
+              </div>
+
+              <div className={ classes.set }>
+                <img src={guess.set_image}/>
+                <p>{guess.released_at}</p>
+                <Arrow direction={comparison.released_at} />
+              </div>
+
+              <div className={ classes.colors }>
+                <p>Colors</p>
+                <p>{guess.colors.join(", ")}</p>
+                <p>{`${comparison.colors.remaining}`}</p>
+                <p>comp: {comparison.colors.matching.join(", ")}</p>
+              </div>
+            </div>
           </div>
         ))
       }
-    </>
+    </div>
   )
 }
 
