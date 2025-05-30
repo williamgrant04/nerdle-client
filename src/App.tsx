b9a12792b87ff65da0eb193548f3aaa8e77d9b89
William Grant <williamg3575467@gmail.com>
2025-05-30 16:39:43 +0100

    Reset stored guesses when card updates in api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Guesses from "./components/Guesses/Guesses"
 import GuessForm from "./components/GuessForm/GuessForm"
+import axios from "axios"
+import guessContext from "./context/GuessContext"
 
 const App = () => {
   const [won, setWon] = useState<boolean>(false)
+  const { setGuesses } = useContext(guessContext)
+
+  useEffect(() => {
+    const date = localStorage.getItem("date")
+    axios.get("http://localhost:3000/date")
+      .then(({ data }) => {
+        if (!date || date && new Date(data.date).getUTCDate() === new Date(date).getUTCDate()) return;
+        console.info("Date change")
+        localStorage.setItem("date", data.date)
+        localStorage.setItem("guesses", "[]")
+        setGuesses([])
+        setWon(false)
+      })
+      .catch((_error) => {})
+  }, [])
 
   useEffect(() => {
     if (won) {
