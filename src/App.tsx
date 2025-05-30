ee03dfd2e72b0f99f22f8798874661a7cfe0391b
William Grant <williamg3575467@gmail.com>
2025-05-30 20:38:49 +0100

    refactor guess amount bar for SC

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Guesses from "./components/Guesses/Guesses"
 import GuessForm from "./components/GuessForm/GuessForm"
 import axios from "axios"
 import guessContext from "./context/GuessContext"
+import styled from "styled-components"
 
 const App = () => {
   const [won, setWon] = useState<boolean>(false)
@@ -41,14 +42,45 @@ const App = () => {
 
   return (
     <>
-      <div style={{ width: "100%", height: "25px", display: "grid", gridTemplateColumns: "1fr", gridTemplateRows: "1fr" }}>
-        <div style={{ width: `${(guesses.length / 20) * 100}%`, gridArea: "1 / 1 / 1 / 2", height: "100%", backgroundColor: won ? "#9bd3ae" : "#f9aa8f", borderRadius: (guesses.length / 20) * 100 === 100 ? "0" : "0 16px 16px 0", transition: "1s" }}></div>
-        <p style={{ margin: 0, gridArea: "1 / 1 / 1 / 2" }}>{guesses.length}/20</p>
-      </div>
+      <GuessBar>
+        <Bar $width={(guesses.length/20) * 100} $won={won}/>
+        <Amount>{guesses.length}/20</Amount>
+      </GuessBar>
       <GuessForm won={won} lost={lost} />
       <Guesses colorblind={false} setWon={setWon} />
     </>
   )
 }
+
+const GuessBar = styled.div`
+  width: 100%;
+  height: 25px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`
+
+const Bar = styled.div<{ $width: number, $won: boolean }>`
+  width: ${({ $width }) => `${$width}%`};
+  grid-area: 1 / 1 / 1 / 2;
+  height: 100%;
+  background-color: ${({ $won }) => ($won ? "#9bd3ae" : "#f9aa8f")};
+  border-radius: ${({ $width }) => ($width === 100 ? "0" : "0 16px 16px 0")};
+  transition: 1s;
+`
+
+const Amount = styled.p`
+  margin: 0;
+  grid-area: 1 / 1 / 1 / 2;
+  text-align: center;
+  font-size: 1.2rem;
+  color: white;
+  mix-blend-mode: difference;
+`
 
 export default App
